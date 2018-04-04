264bd67e6816cdbd7f4f70fcb467cbcbbb9f0abe
Solomon Barkley <barkley@g.harvard.edu>
2018-04-04 18:12:02 -0400

    remove include from tmatrix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 set FL=holopy\scattering\theory\tmatrix_f\
 flang -o %FL%S.exe %FL%S.lp.f %FL%lpq.f
-DEL %FL%*.o
 
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
