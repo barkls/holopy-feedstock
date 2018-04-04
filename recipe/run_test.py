f1805910ed965b1505f249f091905a8e6d060474
barkls <barkley@g.harvard.edu>
2018-04-04 13:39:08 -0400

    re-enable tmatrix test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,5 +3,5 @@ import holopy
 #Confirm main Fortran code compiled properly
 from holopy.scattering.theory.mie_f import mieangfuncs, scsmfo_min
 #Confirm Tmatrix Fortran code compiled properly
-#from holopy.scattering import Tmatrix
-#Tmatrix()
+from holopy.scattering import Tmatrix
+Tmatrix()
