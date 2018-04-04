9312b2abd8445ee0037e61cd19d4341d26f2c68b
Solomon Barkley <barkley@g.harvard.edu>
2018-04-04 17:50:35 -0400

    update bld.bat. Iterate hp version

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
-flang -o holopy/scattering/theory/tmatrix_f/S.exe holopy/scattering/theory/tmatrix_f/S.lp.f holopy/scattering/theory/tmatrix_f/lpq.f
+set FL=holopy\scattering\theory\tmatrix_f\
+flang -o %FL%S.exe %FL%S.lp.f %FL%lpq.f
+DEL %FL%*.o
+
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
