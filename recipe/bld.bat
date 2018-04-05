7df6029a7400dbb287bec729474b492d88ef34b4
Solomon Barkley <barkley@g.harvard.edu>
2018-04-05 09:44:22 -0400

    update file extensions in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 set FL=holopy\scattering\theory\tmatrix_f\
-flang -o %FL%S.exe %FL%S.lp.f %FL%lpq.f
-
+flang -o %FL%S.exe %FL%S.lp.for %FL%lpq.for
+if errorlevel 1 exit 1
 python setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
